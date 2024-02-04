381683bf70b574e60ab9f5b03bcf40df3a3f7852
NdforUofT <156362206+NdforUofT@users.noreply.github.com>
2024-02-04 18:16:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lab1
+ Website for Lab 1 assignment.
---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("my-button");
+
+btn.addEventListener("click", function(){
+    alert ("You clicked me!");
+})
+// there exists a 'click' event for the <button> element; the event listener fires and checks for the name and function of the event
